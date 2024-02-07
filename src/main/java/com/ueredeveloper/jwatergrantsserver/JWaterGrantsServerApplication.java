8863caf2fbb37b006384c70c72a329fcea3e01f5
ueredeveloper <ueredeveloper@gmail.com>
2024-02-07 09:05:48 -0300

    all folders

---
+++ src/main/java/com/ueredeveloper/jwatergrantsserver/JWaterGrantsServerApplication.java
@@ -0,0 +1,13 @@
+package com.ueredeveloper.jwatergrantsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JWaterGrantsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JWaterGrantsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ueredeveloper/jwatergrantsserver/JWaterGrantsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ueredeveloper.jwatergrantsserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JWaterGrantsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
